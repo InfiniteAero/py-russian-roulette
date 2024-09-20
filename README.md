c2251ee0769b82f13366ca82daee4ccd77621470
Edwin Tran <edwinachieve@gmail.com>
2024-09-19 20:20:53 -0400

    completed base functionality

---
+++ README.md
@@ -0,0 +1,1 @@
+# Python Russian Roulette
--- src/main.py
+++ src/main.py
@@ -1,16 +1,31 @@
 import random
+import time
+from rich.console import Console
 
+console = Console()
 rounds_left = 6
 
-print("Russian Roulette")
+console.clear()
+
+console.print("Russian Roulette")
+input("Press [ENTER] to begin")
+console.clear()
 input("Load a round [ENTER]")
-print("Spinning chamber...")
+console.print("Spinning chamber...")
+time.sleep(1)
 while True:
     input("Pull the trigger [ENTER]")
     if random.randint(1, rounds_left) == 1:
-        print("BANG")
-        print("YOU DED")
-        break
+        with console.status("...silence...", spinner='line'):
+            time.sleep(1)
+            console.print("BANG")
+            console.print("[bold red]YOU DED[/]")
+            break
     else:
-        rounds_left -= 1
-        print("Safe! 1/" + str(rounds_left) + " chance for next player to die")
+        with console.status("...silence...", spinner='line'):
+            rounds_left -= 1
+            time.sleep(1)
+            console.print("click!")
+            time.sleep(0.5)
+            console.clear()
+            console.print("Safe! 1/" + str(rounds_left) + " chance for next player to die")
